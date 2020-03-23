bd3cc98ca0bf54ad0e6ce9a8d756e04c1dea605e
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-03-22 20:14:41 -0400

    more styling done

--- script.js
+++ script.js
@@ -1,46 +1,24 @@
 const url = 'https://api.nasa.gov/planetary/apod?api_key=xjgFw6a1av9NLLfgqi1a6AfHLlaGQxYw9LwMuUoW'
-const newURL = 'https://api.nasa.gov/planetary/apod?api_key=xjgFw6a1av9NLLfgqi1a6AfHLlaGQxYw9LwMuUoW'
+const newURL = 'https://api.nasa.gov/planetary/apod?api_key=xjgFw6a1av9NLLfgqi1a6AfHLlaGQxYw9LwMuUoW&date='
 
 let dateandpic = document.querySelector(".dateandpic")
 let explanation = document.querySelector(".explanation");
-let button = document.querySelector(".button"); //selects the button to which you want to add functionality.
+let todaybutton = document.querySelector(".button");
 let date = document.querySelector(".date");
 let visuals = document.querySelector(".visuals")
+
+let menu = document.querySelectorAll("aside");
 
 let handleClick = (e) => {
     e.preventDefault();
     console.dir(e);
 
     fetch(url)
-        .then(res => res.json()) //this part gets the data, then converts it to json
-        .then(res => { //we have the data! Now what are we going to do with it? 
-            date.innerHTML = res.date;//fills date div container with date
-            explanation.innerHTML = res.explanation; //fills div container with explanation text.
-            visuals.setAttribute("src", res);
-            console.log(res);//console will log the fetched data
-        })
-
-        .catch(err => {
-            console.log("Ground control to Major Tom?", err); //tells computer what to do in case of error.
-        })
-}
-
-button.addEventListener("click", handleClick); //adds functionality to the button.
-
-let handleSubmit = (e) => {
-    e.preventDefault();
-    console.dir(e);
-    let fullUrl=url+inputElement.value.html
-
-    fetch(url, {
-        headers: {
-            'x-api-key': 'xjgFw6a1av9NLLfgqi1a6AfHLlaGQxYw9LwMuUoW'
-        }
-    })
-
         .then(res => res.json())
         .then(res => {
-            dateandpic.innerHTML = res;
+            date.innerHTML = res.date;
+            explanation.innerHTML = res.explanation;
+            visuals.setAttribute("src", res);
             console.log(res);
         })
 
@@ -49,6 +27,53 @@ let handleSubmit = (e) => {
         })
 }
 
+todaybutton.addEventListener("click", handleClick);
+
+let handleSubmit = (e) => {
+    let fullUrl = newUrl + inputElement.value;
+    e.preventDefault();
+    console.dir(e);
+
+    fetch(fullUrl)
+    .then(res => res.json())
+    .then(res => {
+        dateandpic.innerHTML = res;
+        console.log(res);
+    })
+
+    .catch(err => {
+        console.log("Ground control to Major Tom?", err);
+    })
+}
+
 let inputElement = document.querySelector(".input");
-let goButton = document.querySelector(".go");
 inputElement.addEventListener("submit", handleSubmit);
+
+
+
+// let gallery = document.querySelector(".js-gallery");
+// let images = document.querySelectorAll(".js-gallery-item");
+
+// let slideCount = 4;
+// let slideWidth = images[0].getBoundingClientRect();
+// let currentSlide = 1;
+// let width = slideWidth.width;
+// let delta = (-width * currentSlide);
+
+// function transitionSlide() {
+
+//     delta = (-width * currentSlide);
+//     console.log(delta)
+//     console.log(currentSlide)
+//     console.log(slideCount)
+
+//     if (currentSlide < slideCount) {
+//       gallery.style.transform = `translateX(${delta}px)`;
+//       currentSlide++;
+//     }
+//     else {
+//       gallery.style.transform = `translateX(0px)`;
+//       currentSlide = 1;
+//     }
+//   }
+// setInterval(transitionSlide, 2000);
