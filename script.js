633bbcd19809015ae5ea55ecd2ace70aa60197c7
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-03-22 21:53:56 -0400

    changed HTML

--- script.js
+++ script.js
@@ -40,21 +40,33 @@ let handleClick = (e) => {
 todaybutton.addEventListener("click", handleClick);
 
 let handleSubmit = (e) => {
-    let fullUrl = newUrl + inputElement.value;
     e.preventDefault();
     console.dir(e);
+    let fullUrl = `${newUrl} ${inputElement.value}`;
 
     fetch(fullUrl)
-        .then(res => res.json())
-        .then(res => {
-            dateandpic.innerHTML = res;
-            console.log(res);
-        })
+    .then(res => res.json())
+    .then(res => {
+        if (res.media_type = "video") {
+            let ifrm = document.createElement("iframe");
+            ifrm.setAttribute("src", res.url);
+            visuals.appendChild(ifrm);
+        } else {
+            let pic = document.createElement("image");
+            pic.setAttribute("src", res.url)
+            visuals.appendChild(pic);
+        }
+        date.innerHTML = res.date;
+        explanation.innerHTML = res.explanation;
+        visuals.setAttribute("src", res);
+        console.log(res);
+    })
 
-        .catch(err => {
-            console.log("Ground control to Major Tom?", err);
-        })
+    .catch(err => {
+        console.log("Ground control to Major Tom?", err);
+    })
 }
 
-let inputElement = document.querySelector(".input");
+let form = document.querySelector("form");
+let inputElement = form.querySelector(".input");
 inputElement.addEventListener("submit", handleSubmit);
