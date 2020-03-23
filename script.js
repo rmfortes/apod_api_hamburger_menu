b71727d4c6302de41f5c8b7cf47ab1cccf7e9dda
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-03-23 00:18:52 -0400

    i did my best

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const url = 'https://api.nasa.gov/planetary/apod?api_key=xjgFw6a1av9NLLfgqi1a6AfHLlaGQxYw9LwMuUoW'
 const newURL = 'https://api.nasa.gov/planetary/apod?api_key=xjgFw6a1av9NLLfgqi1a6AfHLlaGQxYw9LwMuUoW&date='
 
-let dateandpic = document.querySelector(".dateandpic")
+
 let explanation = document.querySelector(".explanation");
-let todaybutton = document.querySelector(".button");
+let todaybutton = document.querySelector(".todaybutton");
 let date = document.querySelector(".date");
-let visuals = document.querySelector(".visuals")
+let visuals = document.querySelector(".visuals");
+// let userInput = document.querySelector(".input").value;
 
 let menu = document.querySelectorAll("aside");
-let toggleNavStatus = false;
+
+let menuClick = (e) => {
+    e.preventDefault();
+    e.target.menuButton;
+    console.dir(e);
+
+    for (let i=0; i<menu.length; i++) {
+        menu[i].style.visibility = "visible";
+        menu[i].style.width = "350px";
+    }
+
+}
+
+let menuButton = document.querySelector(".menuButton");
+menuButton.addEventListener("click", menuClick);
 
 let handleClick = (e) => {
     e.preventDefault();
@@ -39,34 +54,35 @@ let handleClick = (e) => {
 
 todaybutton.addEventListener("click", handleClick);
 
-let handleSubmit = (e) => {
-    e.preventDefault();
-    console.dir(e);
-    let fullUrl = `${newUrl} ${inputElement.value}`;
+// let handleSubmit = (e) => {
+//     e.preventDefault();
+//     console.dir(e);
+//     let fullUrl = newUrl+userInput;
+//     console.log(fullURL);
 
-    fetch(fullUrl)
-    .then(res => res.json())
-    .then(res => {
-        if (res.media_type = "video") {
-            let ifrm = document.createElement("iframe");
-            ifrm.setAttribute("src", res.url);
-            visuals.appendChild(ifrm);
-        } else {
-            let pic = document.createElement("image");
-            pic.setAttribute("src", res.url)
-            visuals.appendChild(pic);
-        }
-        date.innerHTML = res.date;
-        explanation.innerHTML = res.explanation;
-        visuals.setAttribute("src", res);
-        console.log(res);
-    })
+//     fetch(fullUrl)
+//     .then(res => res.json())
+//     .then(res => {
+//         if (res.media_type = "video") {
+//             let ifrm = document.createElement("iframe");
+//             ifrm.setAttribute("src", res.url);
+//             visuals.appendChild(ifrm);
+//         } else {
+//             let pic = document.createElement("image");
+//             pic.setAttribute("src", res.url)
+//             visuals.appendChild(pic);
+//         }
+//         date.innerHTML = res.date;
+//         explanation.innerHTML = res.explanation;
+//         visuals.setAttribute("src", res.url);
+//         console.log(res);
+//     })
 
-    .catch(err => {
-        console.log("Ground control to Major Tom?", err);
-    })
-}
+//     .catch(err => {
+//         console.log("Ground control to Major Tom?", err);
+//     })
+// }
 
-let form = document.querySelector("form");
-let inputElement = form.querySelector(".input");
-inputElement.addEventListener("submit", handleSubmit);
+// let form = document.querySelector("form");
+// let inputElement = document.querySelector(".input");
+// inputElement.addEventListener("submit", handleSubmit);
